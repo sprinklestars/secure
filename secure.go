e7be8cacfa2bcca2f1e03c0dff877f018b7f06b9
Lazar Philon <90349764+sprinklestars@users.noreply.github.com>
2021-09-08 19:36:36 +0000

    make project

---
+++ secure.go
@@ -0,0 +1,50 @@
+package secure
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+)
+
+func xor(data []byte, key []byte) []byte {
+	result := make([]byte, len(data))
+	for i, b := range data {
+		k := key[i%len(key)]
+		result[i] = b ^ k
+	}
+	return result
+}
+
+func XorFile(inPath string, outPath string, key []byte) error {
+	f, err := os.Open(inPath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	of, err := os.Create(outPath)
+	if err != nil {
+		return nil
+	}
+	defer of.Close()
+	of.Chmod(0755)
+
+	reader := bufio.NewReader(f)
+	buf := make([]byte, len(key))
+	for {
+		n, err := reader.Read(buf)
+
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			break
+		}
+
+		xorChunk := xor(buf[0:n], key)
+		of.Write(xorChunk)
+	}
+	return nil
+}
